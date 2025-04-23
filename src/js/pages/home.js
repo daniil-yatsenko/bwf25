cab613867d6e25dd6aef8058dff315ccb140b7dc
Daniil <daniil@notdiy.studio>
2025-04-23 13:21:35 +0200

    home showreel & parallax feedback

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -35,8 +35,54 @@ function animateNavBar() {
   );
 }
 
+function animateShowreel() {
+  const thumb = document.querySelector(".hero_showreel-thumb-wrapper");
+  const playIcon = thumb.querySelector(".hero_showreel-play-btn");
+
+  if (!thumb || !playIcon) return;
+
+  thumb.addEventListener("mouseover", () => {
+    gsap.to(playIcon, {
+      scale: 1.2,
+      ease: "power3.inOut",
+      duration: 0.5,
+    });
+  });
+
+  thumb.addEventListener("mouseleave", () => {
+    gsap.to(playIcon, { scale: 1, ease: "power3.inOut", duration: 0.75 });
+  });
+}
+
+function heroParallax() {
+  const hero = document.querySelector(".section_hero");
+  const image = hero.querySelector(".hero_image-wrapper");
+
+  if (!image) return;
+
+  gsap.registerPlugin(ScrollTrigger);
+  const tl = gsap.timeline();
+
+  ScrollTrigger.create({
+    trigger: hero,
+    start: "top top",
+    end: "bottom top",
+    ease: "linear",
+    scrub: 0.5,
+    animation: tl,
+  });
+
+  tl.fromTo(
+    image,
+    { scale: 1.08, ease: "linear" },
+    { scale: 1, ease: "linear" }
+  );
+}
+
 const homeInit = () => {
   animateNavBar();
+  animateShowreel();
+  heroParallax();
 };
 
 export { homeInit };
